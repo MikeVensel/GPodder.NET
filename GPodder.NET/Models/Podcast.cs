2e643727d418549bed691fbc267642473242c08b
Michael Vensel <michael.vensel@gmail.com>
2020-01-26 17:20:11 -0500

    Add tag and podcast objects

---
+++ GPodder.NET/Models/Podcast.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="Podcast.cs" company="MikeVensel">
+// Copyright (c) MikeVensel. All rights reserved.
+// </copyright>
+
+namespace GPodder.NET.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Text.Json.Serialization;
+
+    /// <summary>
+    /// Podcast metadata as provided by gPodder.
+    /// </summary>
+    public class Podcast
+    {
+        /// <summary>
+        /// Gets or sets the url for the podcast.
+        /// </summary>
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title for the podcast.
+        /// </summary>
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description for the podcast.
+        /// </summary>
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of subscribers for the podcast.
+        /// </summary>
+        [JsonPropertyName("subscribers")]
+        public int Subscribers { get; set; }
+
+        /// <summary>
+        /// Gets or sets the logo url for the podcast.
+        /// </summary>
+        [JsonPropertyName("logo_url")]
+        public string LogoUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the website url for the podcast.
+        /// </summary>
+        [JsonPropertyName("website")]
+        public string Website { get; set; }
+
+        /// <summary>
+        /// Gets or sets the gPodder url for the podcast.
+        /// </summary>
+        [JsonPropertyName("mygpo_link")]
+        public string MyGpoLink { get; set; }
+    }
+}
---
+++ GPodder.NET/Models/Tag.cs
@@ -0,0 +1,29 @@
+﻿namespace GPodder.NET.Models
+{
+    using System.Text.Json.Serialization;
+
+    /// <summary>
+    /// Podcast tags available from gPodder.
+    /// </summary>
+    public class Tag
+    {
+        /// <summary>
+        /// Gets or sets the name of the tag.
+        /// </summary>
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the searchable tag string for the tag.
+        /// </summary>
+        [JsonPropertyName("tag")]
+        public string SearchTag { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of occurrances of the tag in the directory
+        /// from gPodder.
+        /// </summary>
+        [JsonPropertyName("usage")]
+        public int Usage { get; set; }
+    }
+}
