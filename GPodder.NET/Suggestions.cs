f2126ff928e4196f1b6f577b044a2e09cab8e4b6
Mike Vensel <mvensel@square-9.com>
2020-01-29 12:15:52 -0500

    Document possible exceptions returned by the suggestions class

--- GPodder.NET/Suggestions.cs
+++ GPodder.NET/Suggestions.cs
@@ -6,6 +6,7 @@ namespace GPodder.NET
 {
     using System;
     using System.Collections.Generic;
+    using System.Net.Http;
     using System.Text.Json;
     using System.Threading.Tasks;
     using GPodder.NET.Models;
@@ -28,6 +29,8 @@ namespace GPodder.NET
         /// </summary>
         /// <param name="count">The number of suggestions to return.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous operation. It will contain a <see cref="IEnumerable{Podcast}"/> if successful.</returns>
+        /// <exception cref="HttpRequestException">Thrown if the request is not successful.</exception>
+        /// <exception cref="JsonException">Thrown if the response content cannot be serialized into a <see cref="IEnumerable{Podcast}"/>.</exception>
         public async Task<IEnumerable<Podcast>> RetrieveSuggestedPodcasts(int count)
         {
             var response = await Utilities.HttpClient.GetAsync(new Uri(
