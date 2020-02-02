80d03d79dc30fd1094b89e4f38d31cab3e502654
Michael Vensel <michael.vense@gmail.com>
2020-02-02 12:46:35 -0500

    Handle more errors from gPodder

--- GPodder.NET/Subscriptions.cs
+++ GPodder.NET/Subscriptions.cs
@@ -7,6 +7,7 @@ namespace GPodder.NET
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Net;
     using System.Net.Http;
     using System.Text.Json;
     using System.Threading.Tasks;
@@ -40,7 +41,7 @@ namespace GPodder.NET
         /// </summary>
         /// <param name="username">Username for the gPodder account.</param>
         /// <param name="deviceId">The device ID for the device.</param>
-        /// <returns>A <see cref="Task"/> representing the asynchronous operation. </returns>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IEnumerable<Podcast>> GetDeviceSubscriptions(string username, string deviceId)
         {
             var response = await Utilities.HttpClient.GetAsync(
@@ -48,16 +49,24 @@ namespace GPodder.NET
             return await this.HandleResponseAsync<IEnumerable<Podcast>>(response);
         }
 
+        /// <summary>
+        /// Upload the current subscription list of the given user to the server.
+        /// </summary>
+        /// <param name="username">Username for the gPodder account.</param>
+        /// <param name="deviceId">The device ID for the device.</param>
+        /// <param name="format">The <see cref="SubUploadFormat"/> of the provided subscriptions.</param>
+        /// <param name="stream">A <see cref="Stream"/> of the subscriptions to upload.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task UploadSubscriptionsOfDevice(string username, string deviceId, SubUploadFormat format, Stream stream)
         {
             var streamContent = new StreamContent(stream);
             var response = await Utilities.HttpClient.PutAsync(
-                new Uri($"{GPodderConfig.BaseApiUrl}/subscriptions/{username}/{deviceId}.{format}"),
+                new Uri($"{GPodderConfig.BaseApiUrl}/subscriptions/{username}/{deviceId}.{format.ToString()}"),
                 streamContent);
             var handledResponse = await this.HandleResponseAsync<string>(response);
             if (!string.IsNullOrEmpty(handledResponse))
             {
-                // according to the gPodder documentation this means an error occurred so todo handle it
+                throw new Exception(handledResponse);
             }
         }
 
@@ -71,10 +80,15 @@ namespace GPodder.NET
             {
                 switch (response.StatusCode)
                 {
-                    case System.Net.HttpStatusCode.NotFound:
+                    case HttpStatusCode.Unauthorized:
+                        throw new InvalidOperationException("The username provided was invalid.");
+                    case HttpStatusCode.NotFound:
                         throw new InvalidDeviceIdException("Invalid device ID");
+                    case HttpStatusCode.BadRequest:
+                        throw new InvalidFormatException("The gPodder server did not accept the format provided.");
                 }
             }
+
             var contentStream = await response.Content.ReadAsStreamAsync();
             return await JsonSerializer.DeserializeAsync<T>(contentStream);
         }
