830fa116291fb2f07232958816781af047c7b51b
Michael Vensel <michael.vensel@gmail.com>
2020-02-02 01:57:40 -0500

    Add subscription upload format enum

---
+++ GPodder.NET/Enumerations/SubUploadFormat.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="Format.cs" company="MikeVensel">
+// Copyright (c) MikeVensel. All rights reserved.
+// </copyright>
+
+namespace GPodder.NET.Enumerations
+{
+    /// <summary>
+    /// Acceptable formats for uploading device subscriptions.
+    /// </summary>
+    public enum SubUploadFormat
+    {
+        /// <summary>
+        /// Standard JSON
+        /// </summary>
+        JSON,
+
+        /// <summary>
+        /// Standard OPML
+        /// </summary>
+        OPML,
+
+        /// <summary>
+        /// Plaintext
+        /// </summary>
+        Plaintext,
+    }
+}
